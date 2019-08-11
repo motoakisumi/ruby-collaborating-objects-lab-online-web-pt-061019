9295b5778e8b5e0d2479dc3445622f7446547f23
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-11 03:46:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
   def self.new_by_filename(str)
     self.new(str.split(" - ")[1])
     artist_object = Artist.find_or_create_by_name(str.split(" - ")[0])
+    artist_object
   end
   
   
