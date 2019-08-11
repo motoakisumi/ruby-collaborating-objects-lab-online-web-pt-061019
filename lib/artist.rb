c5673313e22d446c1082a2b94ac72579467072e4
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-11 03:20:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,8 +18,8 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
-  def add_song
-    @songs << Song.select{|song| song.artist == self}
+  def add_song(song)
+    song.artist = self
   
 end
 
